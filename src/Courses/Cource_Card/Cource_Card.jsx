8538eaba6a2fb6cba8d6f8faa39a9065073b75f2
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-03-25 22:43:38 +0530

    courese finished

--- src/Courses/Cource_Card/Cource_Card.jsx
+++ src/Courses/Cource_Card/Cource_Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Course_data from '../Course_data'
 
-export default function Cource_Card() {
+export default function Cource_Card({showall}) {
+     const Course_show = showall ? Course_data : Course_data.slice(0,13);
     return (
         <>
             <div className='grid xl:grid-cols-5  lg:grid-cols-4  grid-cols-3'>
                 {
-                    Course_data.map((val, ind) => (
-                        <div className='Cor-Card w-full'>
+                    Course_show.map((val, ind) => (
+                        <div key={ind} className='Cor-Card w-full'>
                             <div className='my-10'>
                                 <img src={val.Course} alt={val.Course_icon_name} />
                                 <p className="xl:text-xl my-5 lg:text-xl md:text-lg text-sm mx-auto text-center font-semibold text-black">{val.Course_icon_name}</p>
