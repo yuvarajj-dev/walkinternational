e782a47d3f5baeaa78071d48700d3e5ce373a9ed
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-03-30 15:12:49 +0530

    home page

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -94,7 +94,7 @@ export default function Home() {
                             <button>Search</button>
                         </div>
                     </div>
-                    <Marquee  >
+                    <marquee  >
                         <div className='flex w-52 my-10  ' >
                             {
                                 Images_array.map((val, ind) => (
@@ -102,7 +102,7 @@ export default function Home() {
                                 ))
                             }
                         </div>
-                    </Marquee>
+                    </marquee>
                     <div className='flex mx-auto mt-5'>
                         <div className='flex mx-auto'>
                             <svg class="plane plane-animation" xmlns="http://www.w3.org/2000/svg" width="31" height="68" viewBox="0 0 31 68" fill="none">
