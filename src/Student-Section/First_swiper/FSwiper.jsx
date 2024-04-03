ebdecaa74ab513b5b59692c3d5a20599e176d923
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-04-03 11:44:56 +0530

    dvi

--- src/Student-Section/First_swiper/FSwiper.jsx
+++ src/Student-Section/First_swiper/FSwiper.jsx
@@ -4,9 +4,9 @@ import Student_data from '../Student_data/StudentData';
 export default function FSwiper(props) {
     return (
         <div className='lg:flex xl:flex  Student-Scroll'>
-            <div className='student-bg-div scrolls '>
+            <div className='student-bg-div  '>
                 {Student_data.map((value, index) => (
-                    <div className=''>
+                    <div className='scrolls-5'>
                         <img src={value.Student_picture_path} alt="student-photo" className='swiper-anime' />
                     </div>
                 ))}
